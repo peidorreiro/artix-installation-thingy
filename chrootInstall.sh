9992f63c274c1833a4e0903e1f407336bc9430f9
Jailson <151478938+peidorreiro@users.noreply.github.com>
2026-07-10 19:13:25 -0300

    test

--- chrootInstall.sh
+++ chrootInstall.sh
@@ -134,7 +134,7 @@ chmod 755 /home/"$username"/{.config,.local/share}
 ## XFCE Desktop Environment
 if [ "$formfactor" == 1 ] || [ "$formfactor" == 2 ] || [ "$formfactor" == 3 ]; then
     # Core XFCE packages
-    sudo pacman -S $(pacman -Sgq xfce4 xfce4-goodies xlibre xorg-xinit xterm xclip xdg-desktop-portal xdg-desktop-portal-gtk pipewire pipewire-pulse pipewire-jack pipewire-alsa wireplumber lightdm-openrc lightdm lightdm-gtk-greeter fastfetch htop neovim firefox thunar thunar-volman networkmanager-applet)
+    sudo pacman -S $(pacman -Sgq xfce4 xfce4-goodies xlibre) xorg-xinit xclip xdg-desktop-portal xdg-desktop-portal-gtk pipewire pipewire-pulse pipewire-jack pipewire-alsa wireplumber lightdm-openrc lightdm lightdm-gtk-greeter fastfetch htop neovim firefox thunar thunar-volman networkmanager-applet
  --needed --noconfirm
 
 
